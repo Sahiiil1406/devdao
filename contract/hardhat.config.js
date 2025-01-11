7690d8abeaff3c3ab022130860e4f35419082e7b
Sahil kumar <supersen14@gmail.com>
2025-01-11 17:49:17 +0530

    deploy on polygon

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    zkEVM: {
+    url: `https://rpc.cardona.zkevm-rpc.com`,
+    accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    },
+},
 };
