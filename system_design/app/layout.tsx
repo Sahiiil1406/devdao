e353e0bfe76c02cb78d672a7766e0f051825f0e3
“Debajyoti <debajyotipandit8@gmail.com>
2025-01-11 13:36:46 +0530

    System design setup updated

---
+++ system_design/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "System design puzzles",
+  description:
+    "A place to solve system design puzzles in an interactive manner!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main> {children}</main>
+      </body>
+    </html>
+  );
+}
